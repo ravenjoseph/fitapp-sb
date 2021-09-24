1a188ed2665e13b748e256688636315131dc0dc0
ravenjoseph <raven.josephr@lntinfotech.com>
2021-09-24 23:07:53 +0530

    ADDED OAUTH2
    OAuth2 authentication completed. Manual Registration and user login is in progress. Mysql database creation success. Adding data to the DB is in progress. Have find the solution to get access the Database.

---
+++ fitapp-sb/src/main/java/com/fitapp/fitappsb/FitappSbApplication.java
@@ -0,0 +1,13 @@
+package com.fitapp.fitappsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitappSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitappSbApplication.class, args);
+    }
+
+}
---
+++ fitapp-sb/src/test/java/com/fitapp/fitappsb/FitappSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitapp.fitappsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitappSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
